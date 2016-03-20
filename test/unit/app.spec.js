342f3583c46c1017549f0386a2ed2a6a78ad54c4
pecet66 <pawelczarnik@interia.pl>
2016-03-20 14:11:04 +0100

    Laboratory 4 exercise

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -35,4 +35,66 @@ describe('app', function () {
             expect(app.divide(101, 0)).toEqual(false);
         });
     });
+    describe('getDescendingNumbers function', function () {
+        it(' should return descending numbers', function () {
+            expect(app.getDescendingNumbers(10, 5)).toEqual("10 9 8 7 6 5");
+            expect(app.getDescendingNumbers(20, 1)).toEqual("20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1");
+        });
+        it(' should return false when numberFrom > numberTo', function () {
+            expect(app.getDescendingNumbers(3, 4)).toEqual(false)
+        });
+        it(' should return false when numberFrom = numberTo', function () {
+            expect(app.getDescendingNumbers(3, 3)).toEqual(false)
+        });
+        it(' should return false when numberFrom = "a"', function () {
+            expect(app.getDescendingNumbers('a', 3)).toEqual(false)
+        });
+        it(' should return false when numberTo = "a"', function () {
+            expect(app.getDescendingNumbers(3, 'a')).toEqual(false)
+        });
+    });
+    describe('areaOfTrapezoid function', function () {
+        it(' should return false when any of arguments < 0', function () {
+            expect(app.areaOfTrapezoid(-1, 5, 3)).toEqual(false);
+            expect(app.areaOfTrapezoid(1, -1, 3)).toEqual(false);
+            expect(app.areaOfTrapezoid(1, 5, -1)).toEqual(false);
+        });
+
+        it(' should return false when any of arguments has diffrent type than number', function () {
+            expect(app.areaOfTrapezoid('a', 3, 4)).toEqual(false);
+            expect(app.areaOfTrapezoid(3, 'a', 4)).toEqual(false);
+            expect(app.areaOfTrapezoid(3, 4, 'a')).toEqual(false);
+        });
+        it(' should return area of trapezoid', function () {
+            expect(app.areaOfTrapezoid(4, 4, 2)).toEqual(8);
+            expect(app.areaOfTrapezoid(10, 10, 10)).toEqual(100);
+            expect(app.areaOfTrapezoid(13.7, 15.5, 21.8)).toBeCloseTo(318.28);
+            expect(app.areaOfTrapezoid(13.7, 15, 21.8)).toBeCloseTo(312.83);
+        });
+    });
+    describe('maxarray function', function () {
+        it(' should return max number from array', function () {
+            expect(app.maxArray([2,5,8])).toEqual(8);
+            expect(app.maxArray([7,6,3])).toEqual(7)
+            });
+        it(' should return false when array contain non-numbers', function () {
+            expect(app.maxArray(['a','b','c'])).toEqual(false);
+            expect(app.maxArray([3,5,false])).toEqual(false)
+            });
+    });
+    describe('squareOdd function', function () {
+        it(' should return array of squared odd numbers and unchanged even numbers of passed array', function () {
+            expect(app.squareOdd([4,9,10])).toEqual([4,81,10]);
+            expect(app.squareOdd([7,8,3])).toEqual([49,8,9])
+        });
+        it(' should return array with unchanged even numbers and strings of passed array', function () {
+            expect(app.squareOdd(['a','b','c'])).toEqual(['a','b','c']);
+            expect(app.squareOdd([2,4,8])).toEqual([2,4,8]);
+            expect(app.squareOdd([6,8,10])).toEqual([6,8,10]);
+        });
+        it(' should return false when arguments are diffrent than number and string', function () {
+            expect(app.squareOdd(true,false,'string')).toEqual(false);
+            expect(app.squareOdd([1],2,'string')).toEqual(false);
+        });
+    });
 });
